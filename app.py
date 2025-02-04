12d46ceb58853509b0d9a9087c6abf86d00a23e6
Sarang1995 <sarang.suk04@gmail.com>
2025-02-04 14:16:07 +0530

    as previous

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ st.title("Smartphone Recommendation SYstem")
 
 st.write("Smartphone Recommendation System using multiple recommendation techniques, ensuring personalized and relevant suggestions for users.")
 
-cosine_df = pd.read_csv("cosine_df.csv")
+# cosine_df = pd.read_csv("cosine_df.csv")
 
-tab1, tab2, tab3, tab4 = st.tabs(['Content-Based Filtering','Knowledge Based Filtering', 'Utility Based Filtering', "Hybrid Model"])
+# tab1, tab2, tab3, tab4 = st.tabs(['Content-Based Filtering','Knowledge Based Filtering', 'Utility Based Filtering', "Hybrid Model"])
 
 # with tab1:
 #     st.write("Cosine similarity is a popular metric used to measure the similarity between two non-zero vectors in a multi-dimensional space. It calculates the cosine of the angle between two vectors, which indicates how similar the vectors are in terms of their direction.")
